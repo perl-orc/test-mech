6572453fe9470d8644e583aa9b40659cc2e94ee5
James Edward Daniel <james@codegems.org>
2014-05-21 17:45:17 +0100

    POD, make releasable, rename delete_ to del because it's nicer

--- lib/Test/Mech.pm
+++ lib/Test/Mech.pm
@@ -1,4 +1,7 @@
 package Test::Mech;
+
+# ABSTRACT: Simple PSGI request wrapper tester
+
 use Moo;
 
 has app => (
@@ -38,10 +41,68 @@ sub put {
   $self->request('PUT', $uri, $options);
 }
 
-sub delete_ {
+sub del {
   my ($self, $uri, $options) = @_;
   $self->request('DELETE', $uri, $options);
 }
 
 1
 __END__
+
+=head1 SYNOPSIS
+
+I think the (short but comprehensive) test file says it best:
+
+    use Test::Most;
+    use Test::Mech;
+    use Carp qw(carp cluck confess croak);
+
+	sub app {
+	  my %env = %{(shift)};
+	  my $method = $env{REQUEST_METHOD};
+	  my $uri = $env{REQUEST_URI};
+	  return [$method, $uri];
+	}
+
+	my $mech = Test::Mech->new(app => \&app);
+	eq_or_diff($mech->get('/foo'),['GET', '/foo']);
+	eq_or_diff($mech->post('/bar/foo'),['POST', '/bar/foo']);
+	eq_or_diff($mech->put('/baz/'),['PUT', '/baz/']);
+	eq_or_diff($mech->del('/baz/foo/'),['DELETE', '/baz/foo/']);
+
+	done_testing;
+
+=head1 METHODS
+
+Methods expect (unlisted here) $self, so are generally expected to be invoked with ->.
+
+=head2 new(%kwargs) => Test::Mech
+
+Keyword args:
+  C<app>: The PSGI subref we will be testing
+
+Given a PSGI application, constructs a Test::Mech and returns it
+
+=head2 get($uri: Str, $config: Maybe[HashRef]) => Any
+
+GETs a uri to the app, returning output. C<$config> is an optional hashref that alters parameters in the generated PSGI request. The following is a mapping of keys we search to keys in the PSGI Request hash that will be generated:
+
+    config key      psgi request key  notes
+    ----------      ----------------  -----
+    query           QUERY_STRING
+    server-name     SERVER_NAME
+    server-port     SERVER_PORT
+    server-protocol SERVER_PROTOCOL
+    http_host       HTTP_HOST         Falls back to server-name
+
+=head2 post($uri: Str, $config: Maybe[HashRef]) => Any
+
+POSTs a uri to the app, returning output. C<$config> treated as per C<get>
+
+=head2 put($uri: Str, $config: Maybe[HashRef]) => Any
+
+PUTs a uri to the app, returning output. C<$config> treated as per C<get>
+
+=head2 del($uri: Str, $config: Maybe[HashRef]) => Any
+
+DELETEs a uri to the app, returning output. C<$config> treated as per C<get>
--- t/mech.t
+++ t/mech.t
@@ -1,7 +1,5 @@
 use Test::Most;
-
 use Test::Mech;
-
 use Carp qw(carp cluck confess croak);
 
 sub app {
@@ -11,11 +9,10 @@ sub app {
   return [$method, $uri];
 }
 
-warn "app: ".ref(\&app);
 my $mech = Test::Mech->new(app => \&app);
 eq_or_diff($mech->get('/foo'),['GET', '/foo']);
 eq_or_diff($mech->post('/bar/foo'),['POST', '/bar/foo']);
 eq_or_diff($mech->put('/baz/'),['PUT', '/baz/']);
-eq_or_diff($mech->delete_('/baz/foo/'),['DELETE', '/baz/foo/']);
+eq_or_diff($mech->del('/baz/foo/'),['DELETE', '/baz/foo/']);
 
 done_testing;
